bffd27f4ee2470007860467b1ff1ca7155aba5a8
darmalkhan <d.gulabzoi@gmail.com>
2022-04-20 01:20:58 +0430

    small changes

--- src/components/polling.vue
+++ src/components/polling.vue
@@ -1,19 +1,3 @@
-<!--
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
 <template>
   <div class="bg-yellow-50 py-16 px-2 overflow-hidden sm:px-6 lg:px-8 lg:py-24">
     <div class="relative max-w-xl mx-auto">
